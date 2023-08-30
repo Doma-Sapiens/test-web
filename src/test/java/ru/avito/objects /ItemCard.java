e44dad737d7475f5058cd4fd1f0f8d6b81112142
Maksim Motorin <72763526+Doma-Sapiens@users.noreply.github.com>
2023-08-30 13:25:21 +0300

    Create ItemCard.java

---
+++ src/test/java/ru/avito/objects /ItemCard.java
@@ -0,0 +1,15 @@
+package ru.avito.objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class ItemCard {
+    String title;
+}
