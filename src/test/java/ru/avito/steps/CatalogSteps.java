f71ddc8793b2491159ccf119f9818501b2a40742
Maksim Motorin <72763526+Doma-Sapiens@users.noreply.github.com>
2023-08-30 13:33:29 +0300

    Create CatalogSteps.java

---
+++ src/test/java/ru/avito/steps/CatalogSteps.java
@@ -0,0 +1,24 @@
+package ru.avito.steps;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import ru.avito.objects.ItemCard;
+import ru.avito.pages.CatalogPage;
+
+public class CatalogSteps extends BaseSteps{
+    CatalogPage catalogPage = new CatalogPage();
+
+    public void verifyFavoriteListContainsItemCard(ItemCard itemCard) {
+
+        wait.until(ExpectedConditions.visibilityOf(catalogPage.titleItemCard));
+        boolean isItemCardFound = false;
+        for (WebElement item : catalogPage.titleItemCards) {
+            String actualItemCardTitle = item.getText();
+            if (actualItemCardTitle.contains(itemCard.getTitle())) {
+                isItemCardFound = true;
+                break;
+            }
+        }
+        assertElementPresent(isItemCardFound, "Список объявлений", "Название объявления");
+    }
+}
