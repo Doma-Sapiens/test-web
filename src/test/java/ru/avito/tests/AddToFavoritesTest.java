7d904d09f366ceb171873d0584584583bf98301f
Maxim <maksim@topnlab.ru>
2023-08-31 12:22:00 +0300

    небольшие правки

--- src/test/java/ru/avito/tests/AddToFavoritesTest.java
+++ src/test/java/ru/avito/tests/AddToFavoritesTest.java
@@ -4,13 +4,14 @@ import org.testng.annotations.Test;
 import ru.avito.objects.ItemCard;
 
 public class AddToFavoritesTest extends BaseTest {
-
+    private static final String ITEM_PAGE_URL = "https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363";
+    private static final String FAVORITES_URL = "https://www.avito.ru/favorites";
     ItemCard itemCard = new ItemCard();
     @Test
     public void testAddToFavorites() {
-        driver.get("https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363");
+        driver.get(ITEM_PAGE_URL);
         itemCardSteps.addToFavorites(itemCard);
-        driver.get("https://www.avito.ru/favorites");
+        driver.get(FAVORITES_URL);
         catalogSteps.verifyFavoriteListContainsItemCard(itemCard);
         getSoftAssert().assertAll();
     }
--- src/test/java/ru/avito/tests/TestListener.java
+++ src/test/java/ru/avito/tests/TestListener.java
@@ -28,6 +28,7 @@ public class TestListener implements ITestListener{
             sb.append(line);
             sb.append("\n");
         }
+
         Allure.addAttachment("Логи после падения теста: ", String.valueOf(sb));
 
         List<LogEntry> logs = getDriver().manage().logs().get(LogType.PERFORMANCE).getAll();
@@ -37,47 +38,10 @@ public class TestListener implements ITestListener{
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             JsonParser jp = new JsonParser();
             JsonElement je = jp.parse(le.getMessage());
-
-            if (gson.toJson(je).contains("\"statusCode\": 404"))  logsBrowser.append(gson.toJson(je));
-//            if (gson.toJson(je).contains("\"statusCode\": 400"))  logsBrowser.append(gson.toJson(je));
-//            if (gson.toJson(je).contains("\"statusCode\": 500"))  logsBrowser.append(gson.toJson(je));
-//            if (gson.toJson(je).contains("\"statusCode\": 403"))  logsBrowser.append(gson.toJson(je));
+            if (gson.toJson(je).contains("\"statusCode\": 404")) logsBrowser.append(gson.toJson(je));
         }
         Allure.addAttachment("Запросы после падения теста1: ", logsBrowser.toString());
-
-
-
-//        StringBuilder logsB = new StringBuilder();
-//
-//        for (LogEntry le : logs) {
-//            JSONObject json = new JSONObject(le.getMessage());
-//
-//            JSONObject message = json.getJSONObject("message");
-//
-//            JSONObject params = message.getJSONObject("params");
-
-//            JSONObject response = params.getJSONObject("response");
-
-//            String method = message.getString("method");
-
-//            String messageUrl = response.getString("url");
-
-//            int status = response.getInt("status");
-//
-//            if (status==404)  logsB.append(method);
-//
-//            if (status==404)  logsB.append(messageUrl);
-//
-//            if (status==404)  logsB.append(params);
-//
-//            if (status==404)  logsB.append(response);
-//            JSONObject text = json.getJSONObject("error");
-//            logsB.append(json);
-//        }
-//
-//        Allure.addAttachment("Запросы после падения теста2: ", logsB.toString());
     }
-
     @Override
     public void onTestFailedWithTimeout(ITestResult result) {
         Allure.getLifecycle().addAttachment("Скриншот на месте падения теста", "image/png", "png",
@@ -85,7 +49,6 @@ public class TestListener implements ITestListener{
         Allure.addAttachment("Логи после падения теста: ",
                 String.valueOf(getDriver().manage().logs().get(LogType.BROWSER).getAll()));
     }
-
     @Override
     public void onTestSuccess(ITestResult result) {
         Allure.getLifecycle().addAttachment("Скриншот после успешного прохождения теста", "image/png", "png",
